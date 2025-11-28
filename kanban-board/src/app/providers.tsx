be66d1c274ff69a926b4f72497a528caee2337a2
trishle005 <trishle005@gmail.com>
2025-11-27 23:23:04 -0800

    Fixed providers for Vercel

--- kanban-board/src/app/providers.tsx
+++ kanban-board/src/app/providers.tsx
@@ -5,10 +5,14 @@ import { NhostProvider } from '@nhost/react'
 import { NhostApolloProvider } from '@nhost/react-apollo'
 import { nhost } from '@/lib/nhost'
 
-export default function Providers({ children }: { children?: ReactNode }) {
+type ProvidersProps = {
+  children: ReactNode
+}
+
+export default function Providers({ children }: ProvidersProps) {
   return (
     <NhostProvider nhost={nhost}>
-      <NhostApolloProvider nhost={nhost}>
+      <NhostApolloProvider nhost={nhost as any}>
         {children}
       </NhostApolloProvider>
     </NhostProvider>
