5a28e993e665650ebc482322bf3c59ae0d38a1ed
trishle005 <trishle005@gmail.com>
2025-11-27 22:44:58 -0800

    added graphql-codegen dependency

---
+++ src/components/Counter.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+type CounterProps = {
+    initialCount?: number;
+};
+
+export default function Counter({initialCount = 0}: CounterProps) {
+    const [count, setCount] = useState(initialCount);
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            </div>
+    );
+}
+
